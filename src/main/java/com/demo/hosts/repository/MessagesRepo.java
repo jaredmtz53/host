4a964e57a4ee595f8151ddf304ee3327e6d61251
Eduardo <eduardoherrerabarra@gmail.com>
2025-03-31 23:24:23 -0400

    Fixing once again

--- src/main/java/com/demo/hosts/repository/MessagesRepo.java
+++
@@ -1,8 +0,0 @@
-package com.demo.hosts.repository;
-import com.demo.hosts.model.Messages;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface MessagesRepo extends JpaRepository<Messages,Integer> {
-    //stuff
-}
